57713eb395dbb12cee60c4fa45a8feec3c1680bf
taleh <taleh.gurjanov@gmail.com>
2021-12-21 19:42:45 +0300

    added header component

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import data from "../../utils/data";
+import Header from "../header/header";
+
+function App() {
+  return (
+    <main className="app">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
